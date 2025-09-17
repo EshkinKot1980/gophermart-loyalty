51150f45ac8cc925dbba4b49e73ffeddef846029
Aleksey Kamyrin <akamyrin@yandex.ru>
2025-09-17 02:07:24 +0000

    balance добавил получение текущего баланса пользователя

---
+++ internal/api/handler/balance.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/EshkinKot1980/gophermart-loyalty/internal/api/dto"
+)
+
+type BalanceService interface {
+	UserBalance(ctx context.Context) (dto.Balance, error)
+}
+
+type Balance struct {
+	service BalanceService
+	logger  Logger
+}
+
+func NewBalance(srv BalanceService, l Logger) *Balance {
+	return &Balance{service: srv, logger: l}
+}
+
+func (b *Balance) UserBalance(w http.ResponseWriter, r *http.Request) {
+	balance, err := b.service.UserBalance(r.Context())
+	if err != nil {
+		http.Error(w, "oops, something went wrong", http.StatusInternalServerError)
+	}
+
+	writeJSON(balance, "balance", http.StatusOK, w, b.logger)
+}
